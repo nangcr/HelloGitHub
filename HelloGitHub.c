8506ec0a72c8d82d6232653d1e24bbe4b409a693
Nangcr <nangcr@gmail.com>
2014-12-11 17:16:58 +0800

    Create new file

---
+++ HelloGitHub.c
@@ -0,0 +1,12 @@
+/*This is a C program
+  HelloGitHub.c
+  Nangcr
+  2014.12.11
+  First code in GitHub*/
+  #include<stdio.h>
+  
+  int main(void)
+  {
+    printf("Hello GitHub!\n");
+    return 0;
+  }
